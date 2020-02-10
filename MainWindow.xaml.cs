72bf80a238af665efedbac57cc543ff5e663cb63
moon791017 <moon791017@gmail.com>
2020-02-10 17:01:14 +0800

    加入辨識是否有插入卡片

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,80 +31,91 @@ namespace WpfApp1
 
         private void submit_Click(object sender, RoutedEventArgs e)
         {
-            using (var ctx = ContextFactory.Instance.Establish(SCardScope.User))
+            try
             {
-                var firstReader = ctx
-                    .GetReaders()
-                    .FirstOrDefault();
-
-                if (firstReader == null)
+                using (var ctx = ContextFactory.Instance.Establish(SCardScope.User))
                 {
-                    System.Diagnostics.Debug.WriteLine("No reader connected.");
-                    return;
-                }
+                    var firstReader = ctx
+                        .GetReaders()
+                        .FirstOrDefault();
 
-                using (var isoReader = new IsoReader(context: ctx, readerName: firstReader, mode: SCardShareMode.Shared, protocol: SCardProtocol.Any))
-                {
-                    var selectApdu = new CommandApdu(IsoCase.Case4Short, isoReader.ActiveProtocol)
+                    if (firstReader == null)
                     {
-                        CLA = 0x00,
-                        INS = 0xA4,
-                        P1 = 0x04,
-                        P2 = 0x00,
-                        Data = new byte[] { 0xD1, 0x58, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11 },
-                        Le = 0x00
-                    };
+                        System.Diagnostics.Debug.WriteLine("No reader connected.");
+                        return;
+                    }
 
-                    System.Diagnostics.Debug.WriteLine("Send Select APDU command: \r\n{0}", BitConverter.ToString(selectApdu.ToArray()));
-
-                    var selectResponse = isoReader.Transmit(selectApdu);
-                    System.Diagnostics.Debug.WriteLine("SW1 SW2 = {0:X2} {1:X2}", selectResponse.SW1, selectResponse.SW2);
-
-                    var readProfileApdu = new CommandApdu(IsoCase.Case4Short, isoReader.ActiveProtocol)
+                    using (var isoReader = new IsoReader(context: ctx, readerName: firstReader, mode: SCardShareMode.Shared, protocol: SCardProtocol.Any))
                     {
-                        CLA = 0x00,
-                        INS = 0xCA,
-                        P1 = 0x11,
-                        P2 = 0x00,
-                        Data = new byte[] { 0x00, 0x00 },
-                        Le = 0x00
-                    };
+                        var selectApdu = new CommandApdu(IsoCase.Case4Short, isoReader.ActiveProtocol)
+                        {
+                            CLA = 0x00,
+                            INS = 0xA4,
+                            P1 = 0x04,
+                            P2 = 0x00,
+                            Data = new byte[] { 0xD1, 0x58, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11 },
+                            Le = 0x00
+                        };
 
-                    System.Diagnostics.Debug.WriteLine("Send Read Profile APDU command: \r\n{0}", BitConverter.ToString(readProfileApdu.ToArray()));
+                        System.Diagnostics.Debug.WriteLine("Send Select APDU command: \r\n{0}", BitConverter.ToString(selectApdu.ToArray()));
 
-                    var profileResponse = isoReader.Transmit(readProfileApdu);
-                    System.Diagnostics.Debug.WriteLine("SW1 SW2 = {0:X2} {1:X2}", profileResponse.SW1, profileResponse.SW2);
+                        var selectResponse = isoReader.Transmit(selectApdu);
+                        System.Diagnostics.Debug.WriteLine("SW1 SW2 = {0:X2} {1:X2}", selectResponse.SW1, selectResponse.SW2);
 
-                    if (profileResponse.HasData)
-                    {
-                        var data = profileResponse.GetData();
-                        var cardNumber = GetCardNumber(data[..12]);
-                        var cardHolderName = GetHolderName(data[12..32]);
-                        var holderIdNum = GetHolderIdNum(data[32..42]);
-                        var holderBirth = GetHolderBirthStr(data[42..49]);
-                        var holderSex = GetGender(data[49..50]);
-                        var cardIssueDate = GetIssueDate(data[50..57]);
+                        var readProfileApdu = new CommandApdu(IsoCase.Case4Short, isoReader.ActiveProtocol)
+                        {
+                            CLA = 0x00,
+                            INS = 0xCA,
+                            P1 = 0x11,
+                            P2 = 0x00,
+                            Data = new byte[] { 0x00, 0x00 },
+                            Le = 0x00
+                        };
 
-                        System.Diagnostics.Debug.WriteLine("\r\n=== Health Card Content: ===\r\n");
-                        CardNumber.Text = cardNumber;
-                        HolderID.Text = holderIdNum;
+                        System.Diagnostics.Debug.WriteLine("Send Read Profile APDU command: \r\n{0}", BitConverter.ToString(readProfileApdu.ToArray()));
 
-                        HolderSex.Text = holderSex;
+                        var profileResponse = isoReader.Transmit(readProfileApdu);
+                        System.Diagnostics.Debug.WriteLine("SW1 SW2 = {0:X2} {1:X2}", profileResponse.SW1, profileResponse.SW2);
 
-                        HolderBirth.Text = holderBirth;
-                        CardHolderName.Text = cardHolderName;
-                        System.Diagnostics.Debug.WriteLine(
-                            $"Card Number  : { cardNumber }\r\n" +
-                            $"Holder Name  : { cardHolderName }\r\n" +
-                            $"Holder ID    : { holderIdNum }\r\n" +
-                            $"Holder Sex   : { holderSex }\r\n" +
-                            $"Holder Birth : { holderBirth }\r\n" +
-                            $"Card Issue On: { cardIssueDate }");
+                        if (profileResponse.HasData)
+                        {
+                            var data = profileResponse.GetData();
+                            var cardNumber = GetCardNumber(data[..12]);
+                            var cardHolderName = GetHolderName(data[12..32]);
+                            var holderIdNum = GetHolderIdNum(data[32..42]);
+                            var holderBirth = GetHolderBirthStr(data[42..49]);
+                            var holderSex = GetGender(data[49..50]);
+                            var cardIssueDate = GetIssueDate(data[50..57]);
 
-       
+                            System.Diagnostics.Debug.WriteLine("\r\n=== Health Card Content: ===\r\n");
+                            CardNumber.Text = cardNumber;
+                            HolderID.Text = holderIdNum;
+
+                            HolderSex.Text = holderSex;
+
+                            HolderBirth.Text = holderBirth;
+                            CardHolderName.Text = cardHolderName;
+                            CardIssueOn.Text = cardIssueDate;
+                            System.Diagnostics.Debug.WriteLine(
+                                $"Card Number  : { cardNumber }\r\n" +
+                                $"Holder Name  : { cardHolderName }\r\n" +
+                                $"Holder ID    : { holderIdNum }\r\n" +
+                                $"Holder Sex   : { holderSex }\r\n" +
+                                $"Holder Birth : { holderBirth }\r\n" +
+                                $"Card Issue On: { cardIssueDate }");
+
+
+                        }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "錯誤訊息");
+
+
+            }
+
         }
         private static string GetIssueDate(byte[] input)
         {
